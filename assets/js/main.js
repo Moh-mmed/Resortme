4f8dda004595a2decf36935a4f49e3294972defc
mohammedBnomr <moohaboy@gmail.com>
2021-12-17 11:32:51 +0300

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+let navbar = document.querySelector("#navbar")
+
+
+window.onscroll = () => {
+(window.scrollY>10)?navbar.style.backgroundColor="#ffffff5e":navbar.style.backgroundColor = "transparent"}
