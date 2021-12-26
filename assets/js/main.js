a90889a2f2d3aa351edecdd4c32024e0ef12f1a3
mohammedBnomr <moohaboy@gmail.com>
2021-12-26 19:28:20 +0300

    finishing the structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,20 @@
 /*===============      NAVBAR     ================ */
 const navbar = document.querySelector(".header")
-
-
 window.onscroll = () => {
-(window.scrollY>10)?navbar.style.backgroundColor="#ffffff5e":navbar.style.backgroundColor = "transparent"}
+window.scrollY > 10
+  ? (navbar.style.backgroundColor = "#ffffffcc")
+  : (navbar.style.backgroundColor = "transparent");}
 
 
-/*===============      mySwiper     ================ */
-
-// window.onload = () => {
-//     let slidesNumber = 3;
-//     let screenWidth = window.innerWidth;
-//     if (screenWidth < 1290 && screenWidth >= 900) slidesNumber = 2;
-//     else if (screenWidth < 900) slidesNumber = 1;
-//     let swiper = new Swiper(".swiper", {
-//       slidesPerView: slidesNumber,
-//       spaceBetween: 30,
-//       freeMode: true,
-//       pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//       },
-//       navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//       },
-//     });
-// }
-// window.onresize = () => {
-//     let screenWidth = window.innerWidth;
-//     if (screenWidth >= 1290) {
-//         swiper = new Swiper(".swiper", {
-//           slidesPerView: 3,
-//           spaceBetween: 30,
-//           freeMode: true,
-//           pagination: {
-//             el: ".swiper-pagination",
-//             clickable: true,
-//           },
-//           navigation: {
-//             nextEl: ".swiper-button-next",
-//             prevEl: ".swiper-button-prev",
-//           },
-//         });
-//     }
-//     else if (screenWidth < 1290 && screenWidth >= 900) {
-//        swiper = new Swiper(".swiper", {
-//          slidesPerView: 2,
-//          spaceBetween: 30,
-//            freeMode: true,
-//          loop
-//          pagination: {
-//            el: ".swiper-pagination",
-//            clickable: true,
-//          },
-//          navigation: {
-//            nextEl: ".swiper-button-next",
-//            prevEl: ".swiper-button-prev",
-//          },
-//        });
-//     }
-//     else if (screenWidth < 900) {
-//       swiper = new Swiper(".swiper", {
-//         slidesPerView: 1,
-//         spaceBetween: 30,
-//         freeMode: true,
-//         pagination: {
-//           el: ".swiper-pagination",
-//           clickable: true,
-//         },
-//         navigation: {
-//           nextEl: ".swiper-button-next",
-//           prevEl: ".swiper-button-prev",
-//         },
-//       });
-//     }
-// }
-
- let swiper = new Swiper(".swiper", {
+/*===============      mySwiper_one     ================ */
+ const swiper_one = new Swiper(".mySwiper_one", {
    slidesPerView: 1,
    spaceBetween: 45,
    breakpoints: {
-         // when window width is >= 320px
          800: {
              slidesPerView: 2,
              spaceBetween: 30
          },
-         // when window width is >= 480px
          1100: {
              slidesPerView: 3,
              spaceBetween: 30
@@ -102,3 +30,16 @@ window.onscroll = () => {
      prevEl: ".swiper-button-prev",
    },
  });
+
+/*===============      mySwiper_one     ================ */
+  const swiper_two = new Swiper(".mySwiper_two", {
+    effect: "cards",
+    grabCursor: true,
+    cardsEffect: {
+      slideShadows: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
