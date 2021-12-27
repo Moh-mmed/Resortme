62d61ad2149d524a1fd9e4091c4ff55f60eea261
mohammedBnomr <moohaboy@gmail.com>
2021-12-27 13:17:31 +0300

    final touches

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,33 @@
-/*===============      NAVBAR     ================ */
+/*===============      HEADER     ================ */
 const navbar = document.querySelector(".header")
-window.onscroll = () => {
-window.scrollY > 10
-  ? (navbar.style.backgroundColor = "#ffffffcc")
-  : (navbar.style.backgroundColor = "transparent");}
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-toggle");
+const scrollUp = document.querySelector(".scroll-arrow-up");
+const scrollDown = document.querySelector(".scroll-arrow-down");
 
+window.onscroll = () => {
+  let scrollHeight = window.scrollY;
+  scrollHeight > 10
+    ? (navbar.style.backgroundColor = "#ffffffcc")
+    : (navbar.style.backgroundColor = "transparent");
+  
+  scrollHeight >= 900 ? scrollUp.classList.add("show-scroll"): scrollUp.classList.remove("show-scroll");
+}
+
+scrollUp.addEventListener("click", () => window.scrollTo(0, 0));
+scrollDown.addEventListener("click", () => window.scrollTo(0, 683));
+
+/*===============      Navlist Toggle     ================ */
+navToggle.addEventListener('click', () => {
+  if (!navToggle.classList.contains('active')) {
+    navToggle.classList.add('active')
+    navMenu.classList.add("active");
+  }
+  else {
+     navToggle.classList.remove("active");
+     navMenu.classList.remove("active");
+  }
+})
 
 /*===============      mySwiper_one     ================ */
  const swiper_one = new Swiper(".mySwiper_one", {
